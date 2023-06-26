cce46e4531c0eaf8b9a0add12c011425995d9257
TlpAyn <talipayan6035@gmail.com>
2023-06-26 13:22:20 +0200

    Initial commit

---
+++ src/main/java/Ubungen/ArraySum.java
@@ -0,0 +1,2 @@
+package Ubungen;public class ArraySum {
+}
---
+++ src/main/java/Ubungen/FactorialCalculate.java
@@ -0,0 +1,21 @@
+package Ubungen;
+
+public class U_1 {
+
+
+    public static int calculateFaktoriel(int number){
+
+        if(number ==0 || number ==1){
+            return 1;
+
+        }else {
+            return number* calculateFaktoriel(number-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number =5;
+        int factorial= calculateFaktoriel(number);
+        System.out.println("factoriel of"+ number+ "ist = "+ factorial);
+    }
+}
