543e0014f505b6ba907ca5de1afaf17b44a9a17b
TlpAyn <talipayan6035@gmail.com>
2023-06-29 12:04:53 +0200

    ternary

---
+++ src/main/java/Ubungen/Ternary.java
@@ -0,0 +1,20 @@
+package Ubungen;
+
+public class Ternary {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+
+        int biggest= (a>b)?a:b;
+        System.out.println(biggest);
+
+
+
+    }
+
+
+
+
+}
